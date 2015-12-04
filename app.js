0c4e35bf2a390a85d9c819fd1ba1291f2f5d1972
poorani11 <poorani.mgr@gmail.com>
2015-12-04 18:10:11 +0530

    Added a list to display the todo list

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-app.js
+// MODULE
+var todoApp = angular.module('todoApp', []);
+
+// CONTROLLERS
+todoApp.controller('homeController', ['$scope', function($scope){
+    $scope.todos = [ 
+        {done:false,text:'first'},
+        {done:false,text:'second'}
+    ];
+}]);
