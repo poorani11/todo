b3b039dd77ae200176925d563bb7555b6b9dbe0d
poorani11 <poorani.mgr@gmail.com>
2015-12-04 19:19:18 +0530

    Added a feature to move the tasks down the list

--- app.js
+++ app.js
@@ -15,8 +15,18 @@ todoApp.controller('homeController', ['$scope', function($scope){
     $scope.todos.push(newTodo);
     $scope.todoText = '';   
     };
-    
+
     $scope.removeTodo = function(start){
         $scope.todos.splice(start, 1);
+    };
+
+    $scope.move = function(index){
+        if (index === $scope.todos.length - 1){
+            return;
+        }
+        var todo = $scope.todos[index];
+            $scope.todos.splice(index +2 , 0, todo);
+            $scope.todos.splice(index, 1);
+        
     };
 }]);
